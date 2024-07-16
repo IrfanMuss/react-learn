5d45ba41b33ae597bcafda8ed8169d7610f04fd1
IrfanMuss <putraarkhatriseptavio@gmail.com>
2024-07-16 14:18:11 +0700

    Feat : Props

--- src/view/Props.js
+++ src/view/Props.js
@@ -1,19 +1,38 @@
-import Props from "../components/Props";
+// Komponen Marqu menerima props dan menampilkan teks yang diberikan dalam tag <marquee>
+const Marqu = (props) => {
+  return (
+    <div>
+      <marquee>{props.text}</marquee>
+    </div>
+  );
+};
 
+// Komponen Button menerima props dan membuat tombol yang memanggil fungsi ketika diklik
+const Button = (props) => {
+  return (
+    <div>
+      <button onClick={() => props.klik()}>Button Props</button> {/* Tombol yang memanggil props.klik saat diklik */}
+    </div>
+  );
+};
+
+// Komponen utama App
 function App() {
+  // membuat variable text berisi Teks yang akan ditampilkan dalam komponen Marqu
+  const text = "Lorem Ipsum is simply dummy text of the printing and typesetting industry.";
 
-    const Heading = "Ini Props"
-    
-    function clicked(){
-        return alert("ini Button Props");
-    }
+  // function clicked yang akan dipanggil saat tombol diklik
+  function clicked() {
+    return alert("ini Button Props"); // Menampilkan alert ketika tombol diklik
+  }
 
-    return(
-        <div>
-            <Props Head={Heading}/>
-            <button onClick={() => clicked()}>Button Props</button>
-        </div>
-    )
+  return (
+    <div style={{ border: "2px solid black", padding: "1rem" }}>
+      <h3>Ini Props</h3>
+      <Marqu text={text}></Marqu> {/* Menggunakan komponen Marqu dan mengirimkan text sebagai prop */}
+      <Button klik={clicked}></Button> {/* Menggunakan komponen Button dan mengirimkan fungsi clicked sebagai prop */}
+    </div>
+  );
 }
 
 export default App;
