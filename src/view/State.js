908b40e086acc647cbc7d1af326e8877f8664abe
IrfanMuss <putraarkhatriseptavio@gmail.com>
2024-07-17 09:20:16 +0700

    Feat : State

--- src/view/State.js
+++ src/view/State.js
@@ -1,16 +1,23 @@
+// Import useState dari React untuk menggunakan state hooks
 import { useState } from "react";
+import React from 'react';
+import './style.css'
 
+// Komponen State
 function State({ gantiLink }) {
   return (
     <div className="p-4">
-      <ul >
+      <ul>
         <li>
+          {/* Link untuk Home */}
           <a href="#">Home</a>
         </li>
         <li>
+          {/* Link untuk About */}
           <a href="#">About</a>
         </li>
         <li>
+          {/* Link untuk Contact yang dapat berubah berdasarkan nilai gantiLink */}
           <a href="#">{!gantiLink ? "Contact" : gantiLink}</a>
         </li>
       </ul>
@@ -18,18 +25,28 @@ function State({ gantiLink }) {
   );
 }
 
+// Komponen utama App
 function App() {
   // Menggunakan hook useState untuk mendefinisikan state getLink dan fungsi setLink untuk mengubah nilai state tersebut
   const [getLink, setLink] = useState("");
+  // Menggunakan hook useState untuk mendefinisikan state Angka dan fungsi setAngka untuk mengubah nilai state tersebut
+  const [Angka, setAngka] = useState(0);
 
-  // function ChangeLink untuk mengubah nilaii state getLink menjadi "Link diubah"
+  // Fungsi ChangeLink untuk mengubah nilai state getLink menjadi "Link diubah"
   function ChangeLink() {
     setLink("Link diubah");
   }
 
+  // Fungsi Tambah untuk menambah nilai state Angka sebesar 1
+  function Tambah() {
+    setAngka(Angka + 1);
+  }
+
   return (
-    <div style={{ border: "2px solid black", padding: "1rem" }}>
-      <h3>Ini State</h3>
+    <div className="box">
+      <h3>State</h3>
+      {/* Button yang memanggil fungsi Tambah ketika diklik dan menampilkan berapa kali button diklik */}
+      <button onClick={() => Tambah()}>Di klik {Angka} Kali</button>
       {/* Menggunakan komponen State dan mengirimkan state getLink sebagai prop gantiLink */}
       <State gantiLink={getLink} />
       {/* Button yang memanggil fungsi ChangeLink ketika diklik */}
