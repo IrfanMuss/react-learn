b738dd7bfed75d796c8b0c1e9f056a19f47a38d3
IrfanMuss <putraarkhatriseptavio@gmail.com>
2024-07-23 10:40:57 +0700

    Fix -> Key Unique props

--- src/Pages/Products.js
+++ src/Pages/Products.js
@@ -34,7 +34,7 @@ const ProductsPage = () => {
   return (
     <div className="flex justify-center py-5">
       {products.map( (products) => (
-        <ProductCard>
+        <ProductCard key={products.id}>
         <ProductCard.Header image={products.image} />
         <ProductCard.Body name={products.name}>
           {products.description}
