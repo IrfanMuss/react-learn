8b24ce03aec02866bbfe9815e28868edc2f6c2b1
IrfanMuss <putraarkhatriseptavio@gmail.com>
2024-07-24 10:12:17 +0700

    Feaat : useState

--- src/Pages/Products.js
+++ src/Pages/Products.js
@@ -1,48 +1,118 @@
 import ProductCard from "../components/Organism/Card/ProductCard";
+import Button from "../components/Atoms/Button/Button";
+import { useState } from "react";
 
 const products = [
   {
     id: 1,
-    name: "Oli TMO",
+    name: "Sepatu Keren",
     image: "/oli.png",
     description: `Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text ever
-          since the 1500s, when an unknown printer took a galley of type and
-          scrambled it to make a type specimen book.`,
-    price: "Rp. 313.000,00",
+          industry. Lorem Ipsum has been the industry's standard dummy text.`,
+    price: 313000,
   },
   {
     id: 2,
-    name: "Oli TMO",
+    name: "Sepatu Biasa",
     image: "/oli.png",
     description: `Lorem Ipsum is simply dummy text of the printing and typesetting
     industry. Lorem Ipsum has been the industry's standard dummy text ever
     since the 1500s.`,
-    price: "Rp. 313.000,00",
+    price: 247000,
   },
   {
     id: 3,
-    name: "Oli TMO",
+    name: "Sepatu Cantik",
     image: "/oli.png",
     description: `Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry..`,
-    price: "Rp. 290.000,00",
+          industry.`,
+    price: 294000,
   },
 ];
 
+const email = localStorage.getItem("email");
+
+// Use State
 const ProductsPage = () => {
+  // State untuk menyimpan daftar item dalam keranjang, diinisialisasi dengan satu item.
+  const [cart, setCart] = useState([
+    {
+      id: 1,
+      qty: 1,
+    },
+  ]);
+
+  // function untuk menangani penambahan item ke keranjang berdasarkan ID item.
+  const handleAddToCart = (id) => {
+    // Memeriksa apakah item dengan ID yang sama sudah ada di keranjang.
+    if (cart.find(item => item.id === id)) {
+      // Jika sudah ada, update jumlah (qty) dari item tersebut.
+      setCart(cart.map(item => item.id === id ? {...item, qty: item.qty + 1} : item));
+    } else {
+      // Jika item dengan ID tersebut belum ada di keranjang, tambahkan item baru dengan qty 1.
+      setCart([...cart, {id, qty: 1}]);
+    }
+  };
+
+  const handleLogout = () => {
+    localStorage.removeItem("email");
+    localStorage.removeItem("password");
+    window.location.href = "/login";
+  };
   return (
-    <div className="flex justify-center py-5">
-      {products.map( (products) => (
-        <ProductCard key={products.id}>
-        <ProductCard.Header image={products.image} />
-        <ProductCard.Body name={products.name}>
-          {products.description}
-        </ProductCard.Body>
-        <ProductCard.Footer price={products.price} />
-      </ProductCard>
-      ))}
-    </div>
+    <>
+      <div className="flex justify-end h-20 bg-blue-700 text-white items-center px-10">
+        {email}
+        <Button classname="bg-red-500 ml-5" onClick={handleLogout}>
+          Logout
+        </Button>
+      </div>
+      <div className="flex justify-center py-5">
+        <div className="flex flex-wrap w-4/6">
+          {products.map((products) => (
+            <ProductCard key={products.id}>
+              <ProductCard.Header image={products.image} />
+              <ProductCard.Body name={products.name}>
+                {products.description}
+              </ProductCard.Body>
+              <ProductCard.Footer
+                price={products.price}
+                id={products.id}
+                handleAddToCart={handleAddToCart}
+              />
+            </ProductCard>
+          ))}
+        </div>
+        <div className="w-2/6">
+          <h1 className="text-3xl font-bold text-blue-600">Cart</h1>
+          <table className="text-left table-auto border-separate border-spacing-x-5">
+            <thead>
+              <tr>
+                <th>Product</th> 
+                <th>Price</th>
+                <th>Quantity</th>
+                <th>Total</th>
+              </tr>
+            </thead>
+            <tbody>
+              {cart.map((item) => {
+                // Mencari produk dengan ID yang sesuai dengan item dalam keranjang
+                const product = products.find((product) => product.id === item.id);
+                return (
+                  <tr key={item.id}>
+                    <td>{product.name}</td>
+                    <td>Rp {product.price.toLocaleString("id-ID", { style: "currency", currency: "IDR" })}</td>
+                    <td>{item.qty}</td>
+                    <td>Rp {(item.qty * product.price).toLocaleString("id-ID", { style: "currency", currency: "IDR" })}</td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+
+        </div>
+      </div>
+    </>
   );
 };
 
--- src/components/Organism/Card/ProductCard.js
+++ src/components/Organism/Card/ProductCard.js
@@ -1,8 +1,8 @@
 import Button from "../../Atoms/Button/Button";
-
+// Nested Component
 const ProductCard = ({ children }) => {
   return (
-    <div className="flex flex-col justify-between w-full max-w-sm bg-gray-800 border border-gray-700 rounded-lg shadow mx-3">
+    <div className="flex flex-col justify-between w-full max-w-xs bg-gray-800 border border-gray-700 rounded-lg shadow mx-3">
       {children}
     </div>
   );
@@ -29,11 +29,28 @@ const Body = ({ children, name }) => {
   );
 };
 
-const Footer = ({ price }) => {
+const Footer = ({ price, handleAddToCart, id }) => {
   return (
     <div className="flex items-center justify-between px-5 pb-5 mx-2">
-      <span className="text-xl text-white font-bold">{price}</span>
-      <Button classname="bg-blue-600">Add to card</Button>
+      <span className="text-xl text-white font-bold">
+        Rp {price.toLocaleString("id-ID", { syle: "curency", curency: "IDR" })}
+      </span>
+      <Button classname="bg-blue-600" onClick={() => handleAddToCart(id)}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="size-6"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z"
+          />
+        </svg>
+      </Button>
     </div>
   );
 };
