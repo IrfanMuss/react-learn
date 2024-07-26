705c22474b05a10779b8b19e1b329e2645d37e43
IrfanMuss <putraarkhatriseptavio@gmail.com>
2024-07-26 08:14:19 +0700

    Feat -> Lifecycle

---
+++ src/components/Organism/Counter.js
@@ -0,0 +1,38 @@
+import React from "react";
+
+class Counter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { count: 0 };
+    console.log("constructor");
+  }
+
+  componentDidMount() {
+    this.setState({ count: 1 });
+    console.log("componentDidMount");
+  }
+
+    componentDidUpdate(prevProps, prevState) {
+    console.log("componentDidUpdate");
+    if (this.state.count === 10) {
+      this.setState({ count: 1 });
+    }
+    }
+
+  render() {
+    return (
+      <div className="flex justify-center items-center">
+        <h1 className="text-3xl font-bold mr-4">{this.state.count}</h1>
+        <button
+          className="bg-black text-white font-bold py-2 px-4 rounded"
+          onClick={() => this.setState({ count: this.state.count + 1 })}
+        >
+          +
+        </button>
+        {console.log("render")}
+      </div>
+    );
+  }
+}
+
+export default Counter;
